99406eb9706c972e6030a71587d0fe69bd5bcaaf
Valeria Ragonese <ribesnero80@gmail.com>
2018-11-21 15:01:36 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -38,14 +38,18 @@
   
   class Book
   
-  attr_accessor :author, :page_count, :genre
-
+    attr_accessor :author, :page_count, :genre
+  
     def initialize(title)
-    @title = title
-  end
-
-  def turn_page
-    puts "Flipping the page...wow, you read fast!"
-  end
+      @title = title
+    end
+    
+    def title 
+      @title
+    end
+  
+    def turn_page
+      puts "Flipping the page...wow, you read fast!"
+    end
 end
 
